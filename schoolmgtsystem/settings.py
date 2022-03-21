50769260b75542abc100588635c888a3122be982
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-03-21 16:09:07 +0530

    heroku deployment

--- schoolmgtsystem/settings.py
+++ schoolmgtsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9&ug+#xm-_^mq)ufr1*h-n%x1%)s-xo_$l#834m&t1(94u0=qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
